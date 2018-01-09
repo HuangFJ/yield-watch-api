25725cf3f8de9cdaf37c35e90b4463ce142e2dd9
Jon Huang <biohfj@gmail.com>
2018-01-09 19:20:57 +0800

    attach local config

--- src/api.rs
+++ src/api.rs
@@ -1,9 +1,13 @@
 use utils::request_json;
+use rocket::State;
+use rocket::config::Config;
 
 #[get("/")]
-fn index() -> String {
+fn index(config: State<Config>) -> String {
     // .to_string: &str -> String
     // .as_str: String -> &str
+
+    println!("{:?}", config.get_str("mysql"));
 
     let ret = request_json("https://api.coinmarketcap.com/v1/global/", 5).unwrap();
 
--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,15 @@ extern crate rocket;
 pub mod api;
 mod utils;
 
+use rocket::fairing::AdHoc;
+
 fn main() {
     rocket::ignite()
+        .attach(AdHoc::on_attach(|rocket|{
+            println!("Attaching local config.");
+            let config = rocket.config().clone();
+            Ok(rocket.manage(config))
+        }))
         .mount("/api", routes![api::index])
         .launch();
 }
