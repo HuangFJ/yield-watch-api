d893fc2e2efe539671f2a695bf12181b92874e54
Jon Huang <biohfj@gmail.com>
2018-01-12 18:09:54 +0800

    add mysql support

--- src/worker.rs
+++ src/worker.rs
@@ -6,22 +6,25 @@ pub mod schema;
 pub mod models;
 
 use diesel::prelude::*;
-use models::{Coin};
+use models::Coin;
 
-fn mysql_conn()->MysqlConnection{
+fn mysql_uri() -> String{
     let json = utils::json_from_tomlfile("Rocket.toml");
     let env = match option_env!("ENV") {
         Some(v) => v,
         None => "development",
     };
-    let db_uri = json[env]["mysql"].as_str().unwrap();
-    MysqlConnection::establish(&db_uri)
-        .expect(&format!("Error connecting to {}", json[env]["mysql"]))
+
+    json[env]["mysql"].to_string()
+}
+
+fn mysql_conn(uri: &str) -> MysqlConnection {
+    MysqlConnection::establish(uri).expect(&format!("Error connecting to {}", uri))
 }
 
 fn main() {
-    use schema::coins::dsl::{id, coins};
-    let conn=mysql_conn();
+    use schema::coins::dsl::{coins, id};
+    let conn = mysql_conn(&mysql_uri());
     let coin: Coin = coins.first(&conn).unwrap();
     println!("{:?}", coin);
 }
