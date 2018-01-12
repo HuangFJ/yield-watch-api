14f2ecc577c4700481027f0bd8b72422c61886d3
Jon Huang <biohfj@gmail.com>
2018-01-12 16:44:04 +0800

    update util

--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,23 @@
-use std::io;
-use std::error::Error;
-use futures::{Future, Stream};
-use hyper::{self, Client};
-use hyper_tls::HttpsConnector;
-use tokio_core::reactor::Core;
-use tokio_core::reactor::Timeout;
-use std::time::Duration;
-use serde_json::{self, Value};
-use futures::future::Either;
+extern crate futures;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate serde_json;
+extern crate tokio_core;
+extern crate toml;
 
-pub fn request_json(url: &str, timeout: u64) -> Result<Value, Box<Error>> {
+use std::io::{self, Read};
+use std::fs::File;
+use std::error::Error;
+use std::time::Duration;
+use self::futures::{Future, Stream};
+use self::futures::future::Either;
+use self::hyper::Client;
+use self::hyper_tls::HttpsConnector;
+use self::tokio_core::reactor::{Core, Timeout};
+use self::serde_json::Value as Json;
+use self::toml::Value as Toml;
+
+pub fn request_json(url: &str, timeout: u64) -> Result<Json, Box<Error>> {
     let mut core = Core::new()?;
     let handle = core.handle();
     let timeout = Timeout::new(Duration::from_secs(timeout), &handle)?;
@@ -21,8 +29,12 @@ pub fn request_json(url: &str, timeout: u64) -> Result<Value, Box<Error>> {
         println!("Response: {}", res.status());
 
         res.body().concat2().and_then(move |body| {
-            let v = serde_json::from_slice(&body).unwrap();
-            Ok(v)
+            serde_json::from_slice(&body).map_err(|_| {
+                hyper::Error::Io(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "Fail while converting to json",
+                ))
+            })
         })
     });
 
@@ -40,4 +52,37 @@ pub fn request_json(url: &str, timeout: u64) -> Result<Value, Box<Error>> {
         });
 
     core.run(work).map_err(From::from)
+}
+
+pub fn toml2json(toml: Toml) -> Json {
+    match toml {
+        Toml::String(s) => Json::String(s),
+        Toml::Integer(i) => Json::Number(i.into()),
+        Toml::Float(f) => {
+            let n = serde_json::Number::from_f64(f).expect("Float infinite and nan not allowed");
+            Json::Number(n)
+        }
+        Toml::Boolean(b) => Json::Bool(b),
+        Toml::Array(arr) => Json::Array(arr.into_iter().map(toml2json).collect()),
+        Toml::Table(table) => {
+            Json::Object(table.into_iter().map(|(k, v)| (k, toml2json(v))).collect())
+        }
+        Toml::Datetime(dt) => Json::String(dt.to_string()),
+    }
+}
+
+pub fn json_from_tomlfile(filename: &str) -> Json {
+    let mut input = String::new();
+    File::open(filename)
+        .and_then(|mut file| {
+            file.read_to_string(&mut input)
+                .expect("Can't read file content");
+            Ok(())
+        })
+        .expect(format!("Failed to open file {}", filename).as_str());
+
+    input
+        .parse()
+        .map(toml2json)
+        .expect("Failed while converting toml to json")
 }
