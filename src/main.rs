688a646c4da539df55ad3d5ac2736d39221bd3fc
Jon Huang <biohfj@gmail.com>
2018-01-15 22:45:31 +0800

    implement all aync refreshing

--- src/main.rs
+++ src/main.rs
@@ -32,15 +32,35 @@ fn main() {
 
     let mysql_uri = config.get_str("mysql").unwrap();
     let pool_mysql = mysql::Pool::new(mysql_uri).unwrap();
-    let pool_tx = pool_mysql.clone();
-
+    let pool_tx1 = pool_mysql.clone();
+    let pool_tx2 = pool_mysql.clone();
+    let pool_tx3 = pool_mysql.clone();
+    // 每隔5分钟刷新一次币列表
     thread::spawn(move || loop {
-        match worker::refresh_coins(&pool_tx) {
+        match worker::refresh_coins(&pool_tx1) {
             Ok(_) => (),
             Err(e) => println!("Error while refreshing coins: {}", &*e.to_string()),
         }
-        println!("Sleep 300 secs for next refreshing coins...");
+        println!("Endpoints update every 5 minutes...");
         thread::sleep(time::Duration::from_secs(300));
+    });
+    // 每隔6秒获取一次币的价格历史数据
+    thread::spawn(move||loop {
+        match worker::refresh_prices(&pool_tx2) {
+            Ok(_) => (),
+            Err(e) => println!("Error while refreshing prices: {}", &*e.to_string()),
+        }
+        println!("Please limit requests to no more than 10 per minute...");
+        thread::sleep(time::Duration::from_secs(6));
+    });
+    // 每隔1天刷新一次汇率
+    thread::spawn(move||loop {
+        match worker::refresh_rates(&pool_tx3) {
+            Ok(_) => (),
+            Err(e) => println!("Error while refreshing rates: {}", &*e.to_string()),
+        }
+        println!("Update every day...");
+        thread::sleep(time::Duration::from_secs(86400));
     });
     server
         .manage(models::init_pool(mysql_uri))
--- src/worker.rs
+++ src/worker.rs
@@ -1,16 +1,22 @@
 extern crate time;
 
-use mysql::{self, Pool};
+use mysql::{self, Pool, Value};
 use utils;
-use std::error::Error as StdError;
+use std::error::Error;
 
-pub fn refresh_coins(pool: &Pool) -> Result<(), Box<StdError>> {
+pub fn refresh_rates(pool: &Pool) -> Result<(), Box<Error>> {
+    let json = utils::request_json("https://api.fixer.io/latest?base=USD", None)?;
+
+    pool.prep_exec("REPLACE INTO _cache (k,v,created) VALUES (?,?,?)", ("rates", json.to_string(), time::get_time().sec))?;
+    Ok(())
+}
+
+pub fn refresh_coins(pool: &Pool) -> Result<(), Box<Error>> {
     let json = utils::request_json("https://api.coinmarketcap.com/v1/ticker/?convert=CNY&limit=10000", None)?;
-    let arr = json.as_array().unwrap();
 
     let mut sql_string = String::from("INSERT INTO coins (id,name,symbol,rank,available_supply,total_supply,max_supply) VALUES ");
     let mut params = vec![];
-    for item in arr {
+    for item in json.as_array().unwrap() {
         sql_string.push_str("(?,?,?,?,?,?,?),");
         params.push(item["id"].as_str().unwrap_or(""));
         params.push(item["name"].as_str().unwrap_or(""));
@@ -30,18 +36,59 @@ pub fn refresh_coins(pool: &Pool) -> Result<(), Box<StdError>> {
          ,total_supply=VALUES(total_supply)\
          ,max_supply=VALUES(max_supply)",
     );
-    let mut stmt = pool.prepare(sql_string)?;
-    stmt.execute(params)?;
 
-    let mut stmt = pool.prepare("REPLACE INTO _cache (k,v,created) VALUES (?,?,?)")?;
-    stmt.execute(("coins", json.to_string(), time::get_time().sec))?;
+    pool.prep_exec(sql_string, params)?;
+    pool.prep_exec("REPLACE INTO _cache (k,v,created) VALUES (?,?,?)", ("coins", json.to_string(), time::get_time().sec))?;
 
-    let ret: Vec<(String, String, i32)> = pool.prep_exec("SELECT k,v,created FROM _cache", ()).map(|result|{
-        result.map(|x| x.unwrap()).map(|row|{
-            let (k, v, created) = mysql::from_row(row);
-            (k, v, created)
-        }).collect()
-    })?;
-    println!("{:?}", ret);
+    Ok(())
+}
+
+pub fn refresh_prices(pool: &Pool) -> Result<(), Box<Error>> {
+    let mut result = pool.prep_exec(
+        "SELECT t1.id,t1.last_updated,t2.max_updated \
+        FROM coins t1 \
+        INNER JOIN \
+            (SELECT MIN(last_updated) min_updated,MAX(last_updated) max_updated \
+            FROM coins) t2 \
+        ON t1.last_updated=t2.min_updated \
+        LIMIT 1", ())?;
+    let (id, last_updated, max_updated): (String, i64, i64) = mysql::from_row(result.next().unwrap()?);
+    println!("Checking {} {} {}", id, last_updated, max_updated);
+
+    let now = time::get_time().sec;
+    if now - max_updated > 10 {
+        let start = last_updated * 1000;
+        let end = now * 1000;
+        let json = utils::request_json(&format!("https://graphs.coinmarketcap.com/currencies/{}/{}/{}/", id, start, end), None)?;
+
+        let mut sql_string = String::from("INSERT IGNORE INTO prices (coin_id,price_usd,volume_usd,price_btc,price_platform,created) VALUES ");
+        let mut params = vec![];
+
+        for (idx, price_usd) in json["price_usd"].as_array().unwrap().iter().enumerate(){
+            sql_string.push_str("(?,?,?,?,?,?),");
+
+            let ts = price_usd[0].as_u64().unwrap()/1000;
+            let p_usd = price_usd[1].as_f64().unwrap();
+            let p_btc = json["price_btc"][idx][1].as_f64().unwrap();
+            let v_usd = json["volume_usd"][idx][1].as_f64().unwrap();
+            let p_platform = if json["price_platform"].is_array(){
+                Value::from(json["price_platform"][idx][1].as_f64().unwrap())
+            }else{
+                Value::from("null")
+            };
+
+            params.push(Value::from(&id));
+            params.push(Value::from(p_usd));
+            params.push(Value::from(v_usd));
+            params.push(Value::from(p_btc));
+            params.push(p_platform);
+            params.push(Value::from(ts));
+        }
+        sql_string.pop();
+
+        pool.prep_exec(sql_string, params)?;
+        pool.prep_exec("UPDATE coins SET last_updated=? WHERE id=?", (now, id))?;
+    }
+
     Ok(())
 }
